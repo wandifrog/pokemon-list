5cafe8a7ddb2e4f38c28b13c9f5696ed0c7a14ec
Wandi <wandifrog@gmail.com>
2021-06-11 18:48:02 +0700

    Initial Commit

---
+++ Pokemon List/Pokemon_ListApp.swift
@@ -0,0 +1,17 @@
+//
+//  Pokemon_ListApp.swift
+//  Pokemon List
+//
+//  Created by Wandi on 11/06/21.
+//
+
+import SwiftUI
+
+@main
+struct Pokemon_ListApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
